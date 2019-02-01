1e2f0cee5e072b044c924629882edc58d9bbf120
STATHAM GEORGIE <georgie.statham@gmail.com>
2019-02-01 20:27:15 +0200

    still trying to deploy

--- budget_app/budget_app/settings.py
+++ budget_app/budget_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sfxbg$ty!3v6@cbc5^vqfrw^k1$5i4dtx&1giocfli^ut#9mep'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
