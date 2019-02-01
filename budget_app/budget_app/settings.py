0ee379e1d880858653385486fc5ac63d66696553
STATHAM GEORGIE <georgie.statham@gmail.com>
2019-02-01 19:24:37 +0200

    deploy to Heroku

--- budget_app/budget_app/settings.py
+++ budget_app/budget_app/settings.py
@@ -120,8 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+
+
