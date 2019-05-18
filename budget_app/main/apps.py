1f79fed9677c9c4750eef144fb0d7d35f35a2545
STATHAM GEORGIE <georgie.statham@gmail.com>
2019-05-18 13:00:08 +0300

    add scheduled expenses on start up readded

--- budget_app/main/apps.py
+++ budget_app/main/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     name = 'main'
 
-    # def ready(self):
-    #     from recurring_expenses.add_recurring_expenses import add_scheduled_expenses
-    #     add_scheduled_expenses()
+    def ready(self):
+        from recurring_expenses.add_recurring_expenses import add_scheduled_expenses
+        add_scheduled_expenses()
 
